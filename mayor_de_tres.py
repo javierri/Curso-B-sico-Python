693155ab64db6b7ef29d9d91d3ee15d5690d95e8
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-04-02 20:22:15 -0430

    Update mayor_de_tres.py

--- mayor_de_tres.py
+++ mayor_de_tres.py
@@ -1,1 +1,14 @@
 
+# Mayor de tres números
+
+num1 = int( input ("Numero 1") )
+num2 = int( input ("Numero 2") )
+num3 = int( input ("Numero 3") )
+
+if (num1 > num2 and num1 > num3):
+    print "el mayor es el primero", num1
+else:
+    if (num2 > num3):
+        print "el mayor es el segundo", num2
+    else:
+        print "el mayor es el tercero", num3
