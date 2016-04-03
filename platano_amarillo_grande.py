1583dd0d44102e5fc2b5cb2a31643f0cc9190172
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-04-02 19:45:43 -0430

    Update platano_amarillo_grande.py

--- platano_amarillo_grande.py
+++ platano_amarillo_grande.py
@@ -1,1 +1,12 @@
 
+# Programa que determina si un platano es grande (15 cm o más)
+# y es amarillo 
+
+tam = int( input ("El tama#o del platano") )
+color = input ("Color del platano ")
+
+tam_valido = (tam >= 15)
+color_valido = (color == "Amarillo" or color == "amarillo")
+
+valido = tam_valido and color_valido
+print valido
