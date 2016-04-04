97204bdcb9f20e2ebf690a285e1957cbc84d1834
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-04-04 10:39:07 -0430

    Update repita_los_andes.py

--- repita_los_andes.py
+++ repita_los_andes.py
@@ -1,3 +1,5 @@
+# Mostrar los datos de una lista utilizando la estructura de repetición for-in
+# usar estructura de decisión dentro de un for-in
 
 andes = ["Tachira","Mérida","Trujillo","Barinas"]
 
